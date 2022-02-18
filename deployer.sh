2ab713eeadb486225b2b1a5b507d138c08870c56
TimeEmit <LiamNorris1231853211@gmail.com>
2022-02-18 11:21:43 -0800

    Build test, random, and quantized random models

--- deployer.sh
+++ deployer.sh
@@ -72,8 +72,18 @@ gssh 'git clone https://github.com/fmtlib/fmt.git; mkdir -p fmt/_build && cd fmt
 announce 'Configure & Build Glow in Debug Mode'
 gssh 'mkdir -p build_Debug && cd build_Debug && cmake -G Ninja -DCMAKE_BUILD_TYPE=Debug ../glow && ninja all'
 
-announce 'Configure & Build Glow in Release Mode'
-gssh 'mkdir -p build_Release && cd build_Release && cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ../glow && ninja all'
+# announce 'Configure & Build Glow in Release Mode'
+# gssh 'mkdir -p build_Release && cd build_Release && cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ../glow && ninja all'
 
 announce 'Configure & Build Glow in Release Mode with Bundles'
 gssh 'mkdir -p build_Release && cd build_Release && cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DGLOW_WITH_BUNDLES=ON ../glow && ninja all'
+
+announce 'Compiling a test model'
+gssh './build_Release/bin/model-compiler -backend=CPU -model=./build_Release/tests/models/onnxModels/add_2inputs_3D.onnx -emit-bundle=./bundles-add_2inputs_3D -target=bpf -mcpu=generic -relocation-model=pic -bundle-api=dynamic'
+
+announce 'Compiling an randomly generated model'
+gssh './build_Release/bin/model-compiler -backend=CPU -model=/home/liam/DCGAN-Generator.onnx -emit-bundle=./bundles-DCGAN-Generator-Random -target=bpf -mcpu=generic -relocation-model=pic'
+
+announce 'Compiling an randomly generated model with quantization'
+gssh './build_Release/bin/model-profiler -model=/home/liam/DCGAN-Generator.onnx -dump-profile=profile.yaml -input-dataset=0,rawtxt,dir,/home/liam/rand -relocation-model=pic -verbose'
+gssh './build_Release/bin/model-compiler -backend=CPU -model=/home/liam/DCGAN-Generator.onnx -load-profile=profile.yaml -emit-bundle=./bundles-DCGAN-Generator-Random-quantized -target=bpf -mcpu=generic -verbose -relocation-model=pic'

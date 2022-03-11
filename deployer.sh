4713d82d171d98111da59b1df392f721a296fdcf
TimeEmit <LiamNorris1231853211@gmail.com>
2022-03-11 09:42:25 -0800

    Upgrade memory for the compiler

--- deployer.sh
+++ deployer.sh
@@ -27,12 +27,12 @@ announce 'Building instance'
 e gcloud compute instances create $INSTANCE \
   --project=solana-paint \
   --zone=us-central1-a \
-  --machine-type=e2-medium \
+  --machine-type=e2-standard-2 \
   --network-interface=network-tier=PREMIUM,subnet=default \
   --maintenance-policy=MIGRATE \
   --service-account=914975206125-compute@developer.gserviceaccount.com \
   --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append \
-  --create-disk=auto-delete=yes,boot=yes,device-name=builder-2,image=projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20220213,mode=rw,size=200,type=projects/solana-paint/zones/us-central1-a/diskTypes/pd-balanced \
+  --create-disk=auto-delete=yes,boot=yes,device-name=$INSTANCE,image=projects/ubuntu-os-cloud/global/images/ubuntu-1804-bionic-v20220213,mode=rw,size=200,type=projects/solana-paint/zones/us-central1-a/diskTypes/pd-balanced \
   --no-shielded-secure-boot \
   --shielded-vtpm \
   --shielded-integrity-monitoring \

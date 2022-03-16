79bbf153ffdcb7bbd50b3218d39ad16f9f729a4c
TimeEmit <LiamNorris1231853211@gmail.com>
2022-03-15 22:04:19 -0700

    Effectively communicating with the server

--- src/webserver/index.ts
+++ src/webserver/index.ts
@@ -2,10 +2,39 @@ import express from 'express';
 import * as path from 'path';
 import {fileURLToPath} from 'url';
 
+import {
+    establishConnection,
+    establishPayer,
+    executeOnnx,
+    // reportGreetings,
+} from './solana';
+
+
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.resolve(__dirname, 'dist')));
+app.use('/', express.static(path.resolve(__dirname, 'dist')));
+
+app.use(express.json());
+
+app.post('/img', async (req, res) => {
+  console.log("Let's say hello to a Solana account...");
+
+  // Establish connection to the cluster
+  await establishConnection();
+
+  // Determine who pays for the fees
+  await establishPayer();
+  console.log(req.body);
+
+  // Say hello to an account
+  await executeOnnx(req.body.values);
+
+  // Find out how many times that account has been greeted
+  // await reportGreetings();
+  console.log('Success');
+  res.json({result: [1.0]})
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ src/webserver/solana.ts
@@ -0,0 +1,210 @@
+import { strict as assert } from 'assert';
+import { v4 as uuidv4 } from 'uuid';
+
+import {
+  Keypair,
+  Connection,
+  PublicKey,
+  LAMPORTS_PER_SOL,
+  SystemProgram,
+  TransactionInstruction,
+  Transaction,
+  sendAndConfirmTransaction,
+} from '@solana/web3.js';
+import path from 'path';
+
+import {getPayer, getRpcUrl, createKeypairFromFile} from './utils';
+
+const web3 = require("@solana/web3.js");
+const {struct, seq, u8, u32, f32, ns64} = require("@solana/buffer-layout");
+const {Buffer} = require('buffer');
+const rawKeypair = process.env.PROGRAM_KEYPAIR;
+
+assert.ok(rawKeypair, 'Need to set environment variable PROGRAM_KEYPAIR');
+
+/**
+ * Connection to the network
+ */
+let connection: Connection;
+
+/**
+ * Keypair associated to the fees' payer
+ */
+let payer: Keypair;
+
+/*
+ * For deriving the public key of an account for the program to write to
+ */
+const SEED = 'hello';
+
+/*
+ * Implements the expected payloads struct to request a lift to the compute limit
+ */
+const computeBudgetRequestStruct = struct([
+  u8('instruction'),
+  u32('units'),
+  u32('additional_fee'),
+]);
+
+/*
+ * Implements the expected payload struct to send into the DCGAN program:
+ * An array of 10 floats
+ */
+const dcganPayloadStruct = seq(f32(), 10);
+
+/*
+ * Implements the expected payload struct to send into the DCGAN program:
+ * A vector of 3 (color channel) x 8 (width) x 8 (heigth) floats flattened into a single array
+ */
+const dcganResultStruct = seq(f32(), 192);
+
+/**
+ * Establish a connection to the cluster
+ */
+export async function establishConnection(): Promise<void> {
+  const rpcUrl = await getRpcUrl();
+  connection = new Connection(rpcUrl, 'confirmed');
+  const version = await connection.getVersion();
+  console.log('Connection to cluster established:', rpcUrl, version);
+}
+
+/**
+ * Establish an account to pay for everything
+ */
+export async function establishPayer(): Promise<void> {
+  let fees = 0;
+  if (!payer) {
+    const {feeCalculator} = await connection.getRecentBlockhash();
+
+    // Calculate the cost of sending transactions
+    fees += feeCalculator.lamportsPerSignature * 100; // wag
+
+    payer = await getPayer();
+  }
+
+  let lamports = await connection.getBalance(payer.publicKey);
+  if (lamports < fees) {
+    // If current balance is not enough to pay for fees, request an airdrop
+    const sig = await connection.requestAirdrop(
+      payer.publicKey,
+      fees - lamports,
+    );
+    await connection.confirmTransaction(sig);
+    lamports = await connection.getBalance(payer.publicKey);
+  }
+
+  console.log(
+    'Using account',
+    payer.publicKey.toBase58(),
+    'containing',
+    lamports / LAMPORTS_PER_SOL,
+    'SOL to pay for fees',
+  );
+}
+
+async function createAccount(programId: PublicKey): Promise<PublicKey> {
+  console.log('Getting account info');
+  const seed = uuidv4().slice(0, 8);
+  const programAccountPubkey = await PublicKey.createWithSeed(
+    payer.publicKey,
+    seed,
+    programId,
+  );
+
+  const createdAccount = await connection.getAccountInfo(programAccountPubkey);
+  if (createdAccount === null) {
+    console.log(
+      'Creating account',
+      programAccountPubkey.toBase58(),
+      'to persist',
+      dcganResultStruct.span,
+      'bytes into',
+    );
+
+    const lamports = await connection.getMinimumBalanceForRentExemption(
+      dcganResultStruct.span,
+    );
+
+    const transaction = new Transaction().add(
+      SystemProgram.createAccountWithSeed({
+        fromPubkey: payer.publicKey,
+        basePubkey: payer.publicKey,
+        seed,
+        newAccountPubkey: programAccountPubkey,
+        lamports,
+        space: dcganResultStruct.span,
+        programId,
+      }),
+    );
+    await sendAndConfirmTransaction(connection, transaction, [payer]);
+  }
+
+  return programAccountPubkey;
+}
+
+
+/**
+ * Execute Onnx Program
+ */
+export async function executeOnnx(inputVector: Array<number>): Promise<void> {
+  const keypair = Uint8Array.from(JSON.parse(rawKeypair || ''))
+  const programKeypair = await Keypair.fromSecretKey(keypair)
+	const programAccountPubkey = await createAccount(programKeypair.publicKey);
+  console.log('Program account created:', programAccountPubkey.toBase58());
+
+  /*
+   * Establish Compute Budget Payload
+   */
+  let allocateParams = { instruction: 0, units: 1400000, additional_fee: 1 };
+  let allocateData = Buffer.alloc(computeBudgetRequestStruct.span);
+  computeBudgetRequestStruct.encode(allocateParams, allocateData);
+
+  /*
+   * Bump Compute Budget Instruction
+   */
+  const allocate = new web3.TransactionInstruction({
+    keys: [{pubkey: payer.publicKey, isSigner: false, isWritable: false}],
+    programId: 'ComputeBudget111111111111111111111111111111',
+    data: allocateData,
+  });
+
+  /*
+   * Establish Onnx Compute Budget Payload
+   */
+  let onnxParams = inputVector;
+  let onnxData = Buffer.alloc(dcganPayloadStruct.span);
+  dcganPayloadStruct.encode(onnxParams, onnxData);
+
+  /*
+   * Execute Onnx Program
+   */
+  const instruction = new TransactionInstruction({
+    keys: [{pubkey: programAccountPubkey, isSigner: false, isWritable: true}],
+    programId: programKeypair.publicKey,
+    data: onnxData,
+  });
+
+  await sendAndConfirmTransaction(
+    connection,
+    new Transaction().add(allocate).add(instruction),
+    [payer],
+  );
+}
+
+/**
+ * Report the number of times the greeted account has been said hello to
+ */
+// export async function reportGreetings(): Promise<void> {
+//   const accountInfo = await connection.getAccountInfo(greetedPubkey);
+//   if (accountInfo === null) {
+//     throw 'Error: cannot find the greeted account';
+//   }
+//   const greeting = GreetingsSchema.decode(accountInfo.data);
+//
+//   console.log(
+//     greetedPubkey.toBase58(),
+//     'has been greeted',
+//     greeting.counter,
+//     'time(s)',
+//   );
+// }

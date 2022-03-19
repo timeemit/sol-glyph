e6781cc7b247c88e4f3c57ed505a32e5db6dbff1
TimeEmit <LiamNorris1231853211@gmail.com>
2022-03-18 17:29:48 -0700

    Enforce HTTPS

--- src/webserver/index.ts
+++ src/webserver/index.ts
@@ -14,6 +14,14 @@ import {
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use((req, res, next)  =>{
+  // Require HTTPS.  The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+})
+
 app.use('/', express.static(path.resolve(__dirname, 'dist')));
 
 app.use(express.json());
